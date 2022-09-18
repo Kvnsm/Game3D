a35f6ee0308b231467b55f337a8e867a287c7fec
kvnsm <ksam@student.42lyon.fr>
2022-09-18 14:04:50 +0200

    Ajout du sprite player L'écran n'affiche plus un carré blanc, mais un joueur

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,6 @@
 package main;
 
+import entity.Player;
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,7 +10,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16; // 16x16 tile
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale;
+    public final int tileSize = originalTileSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol;
@@ -22,8 +23,9 @@ public class GamePanel extends JPanel implements Runnable{
     int playerY = 100;
     int playerSpeed = 4;
 
-    KeyHandler keyH = new KeyHandler();
-    Thread gameThread;
+    KeyHandler  keyH = new KeyHandler();
+    Thread      gameThread;
+    Player      player = new Player(this, keyH);
 
     public GamePanel() {
 
@@ -74,29 +76,14 @@ public class GamePanel extends JPanel implements Runnable{
 
     public void update(){
 
-        if (keyH.upPressed) {
-            playerY -= playerSpeed;
-        }
-        else if (keyH.downPressed) {
-            playerY += playerSpeed;
-        }
-        else if (keyH.rightPressed) {
-            playerX += playerSpeed;
-        } else if (keyH.leftPressed) {
-            playerX -= playerSpeed;
-
-        }
-
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
 
         super.paintComponent(g);
-
         Graphics2D g2 = (Graphics2D)g;
-
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
         g2.dispose();
 
     }
